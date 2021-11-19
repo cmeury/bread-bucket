fdc37249ef22254dd3ec229a1bca31ac6e4e22a3
Jared <jaredj@gmx.com>
2021-11-18 23:01:23 -0500

    Refactored ORM to use peewee instead of SQLAlchemy

--- app/models.py
+++ app/models.py
@@ -1,26 +1,38 @@
-from sqlalchemy import func
+from peewee import Model, IntegerField, CharField, DateTimeField, datetime, ForeignKeyField
+from datetime import datetime
 
 from app import db
 
+class BaseModel(Model):
+    class Meta:
+        database = db
 
-class Account(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    closed = db.Column(db.Integer)
-    account = db.relationship('Transaction', backref='account', lazy=True)
+class FormattedDateTimeField(DateTimeField):
+    def db_value(self, value):
+        return value
+
+    def python_value(self, value):
+        return datetime.strptime(value[0:19], '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
+
+
+class Account(BaseModel):
+    id = IntegerField(primary_key=True)
+    name = CharField()
+    closed = IntegerField()
 
     def __repr__(self):
         return '<Account %r>' % self.name
 
-
-class Transaction(db.Model):
-    __tablename__ = 'account_transaction'
-    id = db.Column(db.Integer, primary_key=True)
-    amount = db.Column(db.Integer)
-    memo = db.Column(db.Text)
-    notes = db.Column(db.Text)
-    posted = db.Column(db.DateTime(timezone=True), default=func.now())
-    account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
+class Transaction(BaseModel):
+    id = IntegerField(primary_key=True)
+    amount = IntegerField()
+    memo = CharField()
+    notes = CharField()
+    posted = FormattedDateTimeField(default=datetime.now)
+    account = ForeignKeyField(Account, column_name='account_id', backref='transactions')
 
     def __repr__(self):
         return '<Transaction %r>' % self.id
+
+    class Meta:
+        table_name = 'account_transaction'
